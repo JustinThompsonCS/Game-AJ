0db80970e92ba62d1ca35109f4b629d5208c8343
Justin Thompson <aggiebot@yahoo.com>
2018-12-02 16:45:17 -0600

    mInc

--- Game/src/Player.java
+++ Game/src/Player.java
@@ -4,20 +4,32 @@ import java.awt.event.KeyEvent;
 public class Player {
 	private int height;
 	private int width;
-	private int x;
-	private int y;
+	private double x;
+	private double y;
+	private double mInc;
 	private BufferedImage playerSprite;
 	
-	public Player(int h, int w, BufferedImage sprite, int x, int y) {
+	public Player(int h, int w, BufferedImage sprite, double x, double y) {
 		height = h;
 		width = w;
 		playerSprite = sprite;
 		this.x = x;
 		this.y = y;
+		mInc = 1.0;
+	}
+	
+	public Player(int h, int w, BufferedImage sprite, double x, double y, double mI) {
+		height = h;
+		width = w;
+		playerSprite = sprite;
+		this.x = x;
+		this.y = y;
+		mInc = mI;
 	}
 	
 	public void playerMove(MyKeyListener kb) {
-	     //if (kb.isKeyDown(KeyEvent.VK_W)) 
+	     if (kb.isKeyDown(KeyEvent.VK_W)) y -= mInc;
+	     if (kb.isKeyDown(KeyEvent.VK_S)) y += mInc;
 	}
 	
 	public void update(MyKeyListener kb) {
