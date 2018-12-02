5078bfd620dd4779205d509486f2ba418ddecb04
Aaron502 <aaron.wheeler.502@k12.friscoisd.org>
2018-12-02 16:10:43 -0600

    Added MyKeyListener
    Updated update method in Game

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -2,7 +2,12 @@
 public class Game {
 	
 	public void update() {  //Updates variables and runs methods
-		
+		long startTime = System.currentTimeMillis();
+		/*
+		 * Do update stuff
+		 */
+		long endTime = System.currentTimeMillis();
+		while (endTime - startTime < 5) { endTime = System.currentTimeMillis(); }		
 	}
 	
 }
---
+++ Game/src/MyKeyListener.java
@@ -0,0 +1,33 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class MyKeyListener implements KeyListener {
+	Set<Integer> keyDown;
+	
+	public MyKeyListener() {
+		keyDown = new HashSet<>();
+	}
+	
+	public boolean isKeyDown(int keyCode) {
+		return keyDown.contains(keyCode);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		keyDown.add(e.getKeyCode());
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		keyDown.remove(e.getKeyCode());
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//Ignore
+	}
+
+}
