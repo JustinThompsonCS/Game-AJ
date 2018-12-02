62f4d30079d682db4a159799b50c1db509dcf609
Aaron502 <aaron.wheeler.502@k12.friscoisd.org>
2018-12-02 16:33:51 -0600

    Fixing player errors

--- Game/src/Player123.java
+++ Game/src/Player123.java
@@ -1,18 +1,23 @@
 import java.awt.image.*;
+import java.awt.event.KeyEvent;
 // 
-public class Player {
+public class Player123 {
 	private int height;
 	private int width;
+	private int x;
+	private int y;
 	private BufferedImage playerSprite;
 	
-	public Player(int h, int w, BufferedImage sprite) {
+	public Player123(int h, int w, BufferedImage sprite, int x, int y) {
 		height = h;
 		width = w;
 		playerSprite = sprite;
+		this.x = x;
+		this.y = y;
 	}
 	
 	public void playerMove(MyKeyListener kb) {
-		
+	     //if (kb.isKeyDown(KeyEvent.VK_W)) 
 	}
 	
 	public void update(MyKeyListener kb) {
