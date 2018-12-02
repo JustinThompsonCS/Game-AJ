3c6f710f1dd2135f699e0512db356b43ec08435c
Justin Thompson <aggiebot@yahoo.com>
2018-12-02 16:16:20 -0600

    player1

--- Game/src/Player.java
+++ Game/src/Player.java
@@ -3,5 +3,19 @@ import java.awt.image.*;
 public class Player {
 	private int height;
 	private int width;
-	private BufferedImage playerSprite; 
+	private BufferedImage playerSprite;
+	
+	public Player(int h, int w, BufferedImage sprite) {
+		height = h;
+		width = w;
+		playerSprite = sprite;
+	}
+	
+	public void playerMove(MyKeyListener kb) {
+		
+	}
+	
+	public void update(MyKeyListener kb) {
+		
+	}
 }
